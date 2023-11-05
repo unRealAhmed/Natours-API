daf086d2f9d7a44bafaadd575828ce315b8f288d
Ahmed Sayed <therealahme@gmail.com>
2023-11-06 01:10:48 +0200

    add calculate average rating functionality

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const Tour = require("./tourModel");
 
 const reviewSchema = new mongoose.Schema(
   {
@@ -28,13 +29,62 @@ const reviewSchema = new mongoose.Schema(
   }
 );
 
+// Calculate and update average ratings and ratings quantity for a specific tour
+reviewSchema.statics.calcAverageRatings = async function (tourId) {
+  // Aggregate reviews to calculate average ratings
+  const stats = await this.aggregate([
+    {
+      $match: { tour: tourId }
+    },
+    {
+      $group: {
+        _id: '$tour',
+        nRating: { $sum: 1 },
+        avgRating: { $avg: '$rating' }
+      }
+    }
+  ]);
+
+  // Update the tour with calculated ratings
+  if (stats.length > 0) {
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingsQuantity: stats[0].nRating,
+      ratingsAverage: stats[0].avgRating
+    });
+  } else {
+    // If there are no reviews, set default ratings
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingsQuantity: 0,
+      ratingsAverage: 4.5
+    });
+  }
+};
+
+reviewSchema.post('save', function () {
+  // Calculate and update ratings when a review is saved
+  this.constructor.calcAverageRatings(this.tour);
+});
+
+// Middleware executed before finding and updating a review, stores original review
+reviewSchema.pre(/^findOneAnd/, async function (next) {
+  this.originalReview = await this.model.findOne(this.getQuery()); //retrieve the current review
+  next();
+});
+
+reviewSchema.post(/^findOneAnd/, async function () {
+  // Calculate and update ratings after finding and updating a review
+  await this.originalReview.constructor.calcAverageRatings(this.originalReview.tour);
+});
+
+
+// Virtual populate
 reviewSchema.pre(/^find/, function (next) {
   this.populate({
     path: 'user',
     select: 'name photo'
   });
   next();
-});
+})
 
 const Review = mongoose.model("Review", reviewSchema);
 
