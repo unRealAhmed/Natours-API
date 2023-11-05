6b8da136b47e22606dc265048586e2ebe80cc18d
Ahmed Sayed <therealahme@gmail.com>
2023-11-05 02:54:20 +0200

    add some security features

--- app.js
+++ app.js
@@ -1,11 +1,37 @@
 require('dotenv').config({ path: './config.env' });
 const express = require('express');
+const rateLimit = require("express-rate-limit");
+const mongoSanitize = require("express-mongo-sanitize");
+const helmet = require("helmet");
+const xss = require("xss-clean");
+const hpp = require("hpp");
 const errorController = require('./Controllers/errorController');
 
 const app = express();
 
 // Middleware
-app.use(express.json());
+app.use(express.json({ limit: '100kb' }));
+app.use(helmet());
+app.use(mongoSanitize());
+app.use(xss());
+app.use(
+  hpp({
+    whitelist: [
+      "duration",
+      "maxGroupSize",
+      "difficulty",
+      "ratingsAverage",
+      "ratingsQuantity",
+      "price",
+    ],
+  })
+);
+const limiter = rateLimit({
+  max: 200,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!'
+});
+app.use('/api', limiter);
 
 // Routers
 const tourRouter = require('./routes/tourRoutes');
