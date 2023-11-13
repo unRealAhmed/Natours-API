e7a4107a654fdcdbbe9698bba11d109467523e2b
Ahmed Sayed <therealahme@gmail.com>
2023-11-13 02:11:42 +0200

    add indexes to tour and user

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -29,6 +29,8 @@ const reviewSchema = new mongoose.Schema(
   }
 );
 
+reviewSchema.index({ tour: 1, user: 1 }, { unique: true })
+
 // Calculate and update average ratings and ratings quantity for a specific tour
 reviewSchema.statics.calcAverageRatings = async function (tourId) {
   // Aggregate reviews to calculate average ratings
